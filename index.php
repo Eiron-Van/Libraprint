75a817e1eb8c60a8c861e934318296172d1d1924
Eiron-Van <eironbella13@gmail.com>
2025-07-16 14:43:15 +0800

    testing

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     // $user_data = check_login($conn);
 
     if (!isset($_SESSION['username'])) {
-        header("Location: /Login");
+        header("Location: /Login/index.php");
         exit();
     }
 
