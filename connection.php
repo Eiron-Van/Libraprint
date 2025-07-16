52445afaaf911508e8656fdc239a83a3b1ead005
Eiron-Van <eironbella13@gmail.com>
2025-07-16 12:04:03 +0800

    login fix

--- connection.php
+++ connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "login_db";
+$servername = "u817157843_login_db";
 $username = "evr";
 $password = "Red59854";
 $dbname = "users";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if (!$conn) {
