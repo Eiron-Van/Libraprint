3783fcb0e0771d1c48dd7b4564c877d68b2d1151
Eiron-Van <eironbella13@gmail.com>
2025-07-16 11:47:48 +0800

    login fix

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php include("\auth.php"); ?>
+<?php
+    session_start();
+    
+    include("connection.php");
+    include("functions.php");
+
+    $user_data = check_login($conn);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
