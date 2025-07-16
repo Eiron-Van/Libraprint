738626fdef139d265f40a867d598c403071a72d8
Eiron-Van <eironbella13@gmail.com>
2025-07-16 15:10:33 +0800

    testing

--- function.php
+++ function.php
@@ -1,21 +1,21 @@
 <?php
 
-// function check_login($conn){
-//     if(isset($_SESSION['user_id'])){
-//         $id = $_SESSION['user_id'];
-//         $query = "select * from users where user_id = '$id' limit 1";
+function check_login($conn){
+    if(isset($_SESSION['user_id'])){
+        $id = $_SESSION['user_id'];
+        $query = "select * from users where user_id = '$id' limit 1";
 
-//         $result = mysqli_query($conn, $query);
-//         if($result && mysqli_num_rows($result) > 0){
-//             $user_data = mysqli_fetch_assoc($result);
-//             return $user_data;
-//         }
-//     }
+        $result = mysqli_query($conn, $query);
+        if($result && mysqli_num_rows($result) > 0){
+            $user_data = mysqli_fetch_assoc($result);
+            return $user_data;
+        }
+    }
 
-//     //redirect to login
-//     header("Location: /Login");
-//     die;
-// }
+    //redirect to login
+    header("Location: /Login");
+    die;
+}
 
 function random_num($length){
     $text = "";
