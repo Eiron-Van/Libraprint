876fe0b6bf3d17d917a855cab7453bba3d8586e3
Eiron-Van <eironbella13@gmail.com>
2025-09-09 23:10:22 +0800

    search fix

--- Admin/inventory/index.php
+++ Admin/inventory/index.php
@@ -85,7 +85,7 @@ $row_class = true; // for alternating colors
         <h1 class="text-6xl font-serif text-white text-center p-4">Book Inventory Management</h1>
         
         <form action="index.php" method="get" class="w-1/3">
-            <div class="relative mb-4">
+            <div class="relative mb-2">
                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -96,7 +96,7 @@ $row_class = true; // for alternating colors
             </div>
         </form>
 
-        <p><strong><?php echo $num_rows;?></strong> results for '<?php echo $search; ?>'</p>
+        <p class="text-white m-2"><strong><?php echo $num_rows;?></strong> results for '<?php echo $search; ?>'</p>
 
         <div class="overflow-auto overflow-y-auto max-h-[600px] rounded-lg shadow">
             <table class="w-full">
