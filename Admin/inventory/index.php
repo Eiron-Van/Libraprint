d2cdf2aa19c08ee34b982245cbe4703ea02ac902
Eiron-Van <eironbella13@gmail.com>
2025-09-10 16:13:57 +0800

    test

--- Admin/inventory/index.php
+++ Admin/inventory/index.php
@@ -69,7 +69,7 @@ $search = isset($_GET['search']) ? $_GET['search'] : '';
             </div>
         </form>
 
-        <div id="results-count"></div>
+        <div id="results-count" class="m-2 text-white"></div>
         <div id="results" class="overflow-auto max-h-[600px] rounded-lg shadow text-white">
             Loading...
         </div>
