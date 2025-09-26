061c43afb8172ac5e9cc75fb7e31e2a953ae382b
Eiron-Van <eironbella13@gmail.com>
2025-09-26 22:28:20 +0800

    test

--- test/test_mail.php
+++ test/test_mail.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 require 'mailer.php';
 
-if (sendEmail("youremail@gmail.com", "Test User", "Test Email", "<b>Hello</b> this is a test email!")) {
+if (sendEmail("eironbella13@gmail.com", "Test User", "Test Email", "<b>Hello</b> this is a test email!")) {
     echo "✅ Email sent!";
 } else {
     echo "❌ Failed. Check logs.";
