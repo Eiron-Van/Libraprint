f0cb899c7238d5fad8476e31a7dcf42d936387d7
Eiron-Van <eironbella13@gmail.com>
2025-09-27 00:34:52 +0800

    fix vendero directory

--- mailer.php
+++ mailer.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . 'vendor/autoload.php'; // adjust path
+require __DIR__ . '/vendor/autoload.php'; // adjust path
 
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
