b7874fad49ed86e86b4a017c42496c3130e32841
Eiron-Van <eironbella13@gmail.com>
2025-09-26 19:56:36 +0800

    test

--- test/mailer.php
+++ test/mailer.php
@@ -2,6 +2,10 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require __DIR__ . '../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 require __DIR__ . '/../vendor/autoload.php'; // adjust path
 
 function sendEmail($toEmail, $toName, $subject, $bodyHtml) {
@@ -13,7 +17,7 @@ function sendEmail($toEmail, $toName, $subject, $bodyHtml) {
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'libraprint.lucena@gmail.com';  // your Gmail
-        $mail->Password   = 'eszu-fxqy-hqoh-wino';    // 16-char App Password
+        $mail->Password   = $_ENV['APP_PASSWORD'];    // 16-char App Password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
