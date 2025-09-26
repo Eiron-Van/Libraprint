6ef4d494dbe0eb97956a83ec977cb9a42c2b13aa
Eiron-Van <eironbella13@gmail.com>
2025-09-26 19:24:25 +0800

    test php mailer

--- test/test_mail.php
+++ test/test_mail.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require 'mailer.php';
 
 if (sendEmail("youremail@gmail.com", "Test User", "Test Email", "<b>Hello</b> this is a test email!")) {
