bb3d2ea875f4879149428c61594e2571a65c08a9
Eiron-Van <eironbella13@gmail.com>
2025-10-07 22:03:18 +0800

    change include to require on the connection

--- Borrowing/index.php
+++ Borrowing/index.php
@@ -6,7 +6,7 @@ if (isset($_GET['PHPSESSID']) && !empty($_GET['PHPSESSID'])) {
 
 session_start();
 
-include '../connection.php';
+require '../connection.php';
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
