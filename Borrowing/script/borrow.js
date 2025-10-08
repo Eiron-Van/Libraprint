4950416269be2d9626e2b09474444433031957f5
Eiron-Van <eironbella13@gmail.com>
2025-10-08 19:35:42 +0800

    move the php to an another php so it doesnt crash

--- Borrowing/script/borrow.js
+++ Borrowing/script/borrow.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Send barcode to backend
     try {
-      const response = await fetch("borrow_book.php", {
+      const response = await fetch("borrow_function.php", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ barcode }),
