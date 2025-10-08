c9fccf1a7de32ae4b717a0f5816a4fab4fd0e023
Eiron-Van <eironbella13@gmail.com>
2025-10-08 10:43:08 +0800

    added search function and borrow button

--- Borrowing/borrow_book.php
+++ Borrowing/borrow_book.php
@@ -123,8 +123,21 @@ $availableBooks = $conn->query("
                 <h1 class="text-3xl font-bold text-white mb-2 text-center">📚 List of Reserved/Available Books</h1>
             </div>
 
+
+            <div class="flex flex-row items-center justify-between mb-2">
+                <div class="relative w-1/3">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                    <input type="search" id="search" name="search" placeholder="Search..." class="block w-full p-4 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"/>
+                    <!-- <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-[#23304e] hover:bg-[#5c6072] focus:ring-3 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button> -->
+                </div>
+                <button class="text-white bg-[#005f78] hover:bg-[#064358] transition-opacity duration-200 focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-4">Borrow</button>
+            </div>
+            
             <!-- Table -->
-            <!-- Scrollable Container -->
             <div class="overflow-y-auto max-h-[70vh] rounded-xl bg-white text-sm text-gray-800">
 
                 <!-- Header Row -->
