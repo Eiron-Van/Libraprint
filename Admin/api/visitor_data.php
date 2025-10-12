8e31b3cabd7fc41768bfa8313191b02217e58e4f
Eiron-Van <eironbella13@gmail.com>
2025-10-12 08:44:16 +0800

    changed path of connection on visitor data php

--- Admin/api/visitor_data.php
+++ Admin/api/visitor_data.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-include '../Libraprint/connection.php';
+include '../../connection.php';
 header('Content-Type: application/json');
 
 // 1️⃣ Total Visitors This Month
