1b499540ab521b460dc6d6f17e24a2c35d9e62e3
Eiron-Van <eironbella13@gmail.com>
2025-10-12 09:01:33 +0800

    forgot to add the chart.js script (* ￣︿￣)

--- Admin/index.php
+++ Admin/index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="../style.css?v=1.5">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="/Admin/js/chart.js"></script>
     <script src="/Admin/js/script.js"></script>
 
     <title>Libraprint|Admin|Dashboard</title>
