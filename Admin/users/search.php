1641eca3b0e01f6f468ddf16284890d9ead8d2fa
Eiron-Van <eironbella13@gmail.com>
2025-10-16 21:47:49 +0800

    test

--- Admin/users/search.php
+++ Admin/users/search.php
@@ -66,10 +66,10 @@ function highlightTerms(string $text, string $search): string {
 echo "<h1 class='pb-6 text-4xl text-white font-semibold text-center'>Users</h1> 
         <div class='overflow-auto rounded-lg shadow-lg shadow-gray-500/30 max-h-[61.8vh]'>
             <!-- Header -->
-            <div class='grid grid-cols-11 p-2 bg-gray-700 sticky top-0 text-center'>
+            <div class='grid grid-cols-12 p-2 bg-gray-700 sticky top-0 text-center'>
                 <div class='flex justify-center items-center col-span-1'>User ID</div>
                 <div class='flex justify-center items-center col-span-1'>User Name</div>
-                <div class='flex justify-center items-center col-span-1'>Full Name</div>
+                <div class='flex justify-center items-center col-span-2'>Full Name</div>
                 <div class='flex justify-center items-center col-span-1'>Gender</div>
                 <div class='flex justify-center items-center col-span-2'>Address</div>
                 <div class='flex justify-center items-center col-span-1'>Birthday</div>
@@ -80,10 +80,10 @@ echo "<h1 class='pb-6 text-4xl text-white font-semibold text-center'>Users</h1>
 
 if ($usersResult->num_rows > 0){
     while ($row = $usersResult->fetch_assoc()){
-        echo "<div class='grid grid-cols-11 p-2 bg-gray-200 text-xs text-black border-b border-black whitespace-normal break-all'>
+        echo "<div class='grid grid-cols-12 p-2 bg-gray-200 text-xs text-black border-b border-black whitespace-normal break-all'>
                 <div class='flex justify-center items-center min-w-0 p-1 col-span-1'>" . highlightTerms($row['user_id'], $search) . "</div>
                 <div class='flex justify-center items-center min-w-0 p-1 col-span-1'>" . highlightTerms($row['username'], $search) . "</div>
-                <div class='flex justify-center items-center min-w-0 p-1 col-span-1'>" . highlightTerms($row['first_name'] . " " . $row['last_name'], $search) . "</div>
+                <div class='flex justify-center items-center min-w-0 p-1 col-span-2'>" . highlightTerms($row['first_name'] . " " . $row['last_name'], $search) . "</div>
                 <div class='flex justify-center items-center min-w-0 p-1 col-span-1'>" . highlightTerms($row['gender'], $search) . "</div>
                 <div class='flex justify-center items-center min-w-0 p-1 col-span-2'>" . highlightTerms($row['address'], $search) . "</div>
                 <div class='flex justify-center items-center min-w-0 p-1 col-span-1'>" . highlightTerms($row['birthday'], $search) . "</div>
