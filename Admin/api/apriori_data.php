1301407af49098534e085572ae825aa53eba0598
Eiron-Van <eironbella13@gmail.com>
2025-10-25 09:10:38 +0800

    fix sql

--- Admin/api/apriori_data.php
+++ Admin/api/apriori_data.php
@@ -16,9 +16,9 @@ $sql = "
         GROUP_CONCAT(DISTINCT bi.genre ORDER BY bi.genre ASC SEPARATOR ',') AS genres
     FROM book_record br
     JOIN book_inventory bi ON br.book_id = bi.item_id
-    JOIN users u ON br.user_id = u.user_id
+    JOIN users u ON br.user_id = u.id
     WHERE bi.genre IS NOT NULL AND bi.genre != ''
-    GROUP BY u.user_id, DATE(br.read_date)
+    GROUP BY u.id, DATE(br.read_date)
 ";
 
 $result = $conn->query($sql);
