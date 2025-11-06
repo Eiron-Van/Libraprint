7a68f59bdf96c890727dbca3295152883888a9ad
Ajralf123 <151704027+Ajralf123@users.noreply.github.com>
2025-11-06 11:00:03 +0800

    revert everything

--- Registration/fingerprint_verify.js
+++ Registration/fingerprint_verify.js
@@ -10,17 +10,7 @@ function startCapture() {
 
 sdk.onSamplesAcquired = async function (s) {
     try {
-        // try to open the app without navigating away from this page
-        window.location.href = "libraprint-v://session";
-
-        // reload after a short delay so the page refreshes even if the custom scheme returns quickly
-        setTimeout(() => {
-        // normal reload
-        window.location.reload();
-        // or force reload from server:
-        // window.location.reload(true);
-        }, 3000); // 500-1200 ms is a common choice; adjust as needed
-
+        window.open("libraprint-v://session");
     } catch (ex) {
         console.error("Scan Again", ex);
     }
